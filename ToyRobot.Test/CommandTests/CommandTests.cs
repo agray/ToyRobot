fd362144676dac5f45cf584343ce70047607cb4a
Andrew Gray <andrew.paul.gray@gmail.com>
2014-05-10 00:55:07 +1000

    Add command tests

---
+++ ToyRobot.Test/CommandTests/CommandTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System;
+using ToyRobot.Core;
+using ToyRobot.Test.Base;
+
+namespace ToyRobot.Test.CommandTests {
+    public class CommandTests : TestBase {
+        [Test]
+        public void IgnoreMoveWhenUnplaced() {
+            badRobot.Move();
+            Assert.AreEqual(null, badRobot.CurrentPosture);
+        }
+
+        [Test]
+        public void IgnoreTurnLeftWhenUnplaced() {
+            badRobot.Turn(TurnTo.LEFT);
+            Assert.AreEqual(null, badRobot.CurrentPosture);
+        }
+
+        [Test]
+        public void IgnoreTurnRightWhenUnplaced() {
+            badRobot.Turn(TurnTo.RIGHT);
+            Assert.AreEqual(null, badRobot.CurrentPosture);
+        }
+
+        [Test]
+        public void IgnoreReportWhenUnplaced() {
+            String report = badRobot.ReportPosture();
+            Assert.AreEqual(String.Empty, report);
+        }
+    }
+}
